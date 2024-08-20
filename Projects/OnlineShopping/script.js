37edf12a5dd64937f2a1a92c900660343d8b17cb
Simmannaidu Velamala <simmannaiduvelamala7@gmail.com>
2024-08-20 20:58:45 +0530

    Updated category filter functionality and enhanced styling for responsiveness.

--- Projects/OnlineShopping/script.js
+++ Projects/OnlineShopping/script.js
@@ -109,3 +109,11 @@ orderForm.addEventListener("submit", (e) => {
 });
 
 fetchData();
+
+  // Buy Now Button Click Event
+  function navigateToOrderForm() {
+    closeModal(); // Closes the modal
+    showSection("contact"); // Shows the contact section with the order form
+  }
+
+  buyNowButton.addEventListener("click", navigateToOrderForm);
